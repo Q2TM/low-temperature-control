09fd8d9d998a7dcd154551098842e160856f544f
YeYe <yeyethepooh97169@gmail.com>
2025-10-31 15:52:56 +0700

    GPIO_Test, PIDController manual test

--- apps/temperature-control/PIDController.py
+++ apps/temperature-control/PIDController.py
@@ -4,10 +4,14 @@ import os
 import RPi.GPIO as GPIO
 import requests
 
+
 class PIDController:
     PWM_PIN = 18
-    PWM_FREQUENCY = 10  # Hz
+    PWM_FREQUENCY = 0.2  # Hz
     BASE_URL = "http://localhost:8001"
+
+    # For Testing without actual Lakeshore device
+    current_temp = 7
 
     def __init__(self, Kp, Ki, Kd, setpoint):
         self.Kp = Kp
@@ -28,9 +32,11 @@ class PIDController:
 
     def get_temperature(self):
         # TODO Call Lakeshore API to get actual temperature
-        response = requests.get(f"{self.BASE_URL}/api/temp/status")
-        data = response.json()
-        return data["current_temp"]  # Placeholder value
+        # response = requests.get(f"{self.BASE_URL}/api/temp/status")
+        # data = response.json()
+        # return data["current_temp"]  # Placeholder value
+
+        return self.current_temp  # Placeholder for testing
 
     def set_setpoint(self, setpoint):
         self.setpoint = setpoint
@@ -46,19 +52,22 @@ class PIDController:
 def fetch_new_config():
     # TODO Implement actual fetching logic, e.g., from a file or database
     # Now just be manually querying via terminal
+    print("Enter new PID configuration:")
     new_setpoint = float(input("Enter new setpoint (°C): "))
     new_Kp = float(input("Enter new Kp: "))
     new_Ki = float(input("Enter new Ki: "))
     new_Kd = float(input("Enter new Kd: "))
+    new_temp = float(input("Enter current temperature (°C): "))
     return {
         'setpoint': new_setpoint,
         'Kp': new_Kp,
         'Ki': new_Ki,
-        'Kd': new_Kd
+        'Kd': new_Kd,
+        'current_temp': new_temp
     }
 
 
-def update_config_terminal(pid):
+def update_config_from_terminal(pid):
     while True:
         try:
             new_config = fetch_new_config()
@@ -69,9 +78,11 @@ def update_config_terminal(pid):
                 print("Configuration updated.")
                 print(
                     f"New setpoint: {new_config['setpoint']}, Kp: {new_config['Kp']}, Ki: {new_config['Ki']}, Kd: {new_config['Kd']}")
+                PIDController.current_temp = new_config['current_temp']
         except ValueError:
             print("Invalid.")
         time.sleep(10)
+
 
 def update_config_from_api(pid):
     while True:
@@ -88,7 +99,8 @@ def update_config_from_api(pid):
                 print("Failed to fetch config from API.")
         except Exception as e:
             print("Error fetching config from API:", e)
-        time.sleep(10)
+        time.sleep(4)
+
 
 def update_config_from_file(pid, filename="pid_config.txt"):
     last_mtime = 0
@@ -110,7 +122,7 @@ def update_config_from_file(pid, filename="pid_config.txt"):
 
 
 def main():
-    pid = PIDController(1.0, 0.1, 0.05, 0)
+    pid = PIDController(1.0, 0.1, 0.05, 10)
     PWM_PIN = pid.PWM_PIN
     PWM_FREQUENCY = pid.PWM_FREQUENCY
 
@@ -120,7 +132,7 @@ def main():
     pwm.start(0)
 
     config_thread = threading.Thread(
-        target=update_config_from_api, args=(pid,), daemon=True)
+        target=update_config_from_terminal, args=(pid,), daemon=True)
     config_thread.start()
 
     try:
@@ -129,9 +141,11 @@ def main():
             control_signal = pid.PID_update(current_temp)
             duty_cycle = max(0, min(100, control_signal))
             pwm.ChangeDutyCycle(duty_cycle)
+            # print(
+            #     f"Current Temp: {current_temp}, Control Signal: {control_signal}, Duty Cycle: {duty_cycle}")
             print(
-                f"Current Temp: {current_temp}, Control Signal: {control_signal}, Duty Cycle: {duty_cycle}")
-            time.sleep(1)
+                f'Duty Cycle: {duty_cycle:.2f}% | Setpoint: {pid.setpoint}°C | Current Temp: {current_temp}°C')
+            time.sleep(5)
     except KeyboardInterrupt:
         print("Exiting...")
 
