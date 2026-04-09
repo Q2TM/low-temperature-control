175760a9e171798fef2cd1dea65cacd6721f440c
Nutthapat Pongtanyavichai <contact@leomotors.me>
2026-04-10 02:36:28 +0700

    remove unused env from simulator

--- apps/simulator/src/environment.ts
+++ apps/simulator/src/environment.ts
@@ -2,7 +2,6 @@ import { Value } from "@sinclair/typebox/value";
 import { Static, t } from "elysia";
 
 export const environmentSchema = t.Object({
-  DATABASE_URL: t.String(),
   LGG_URL: t.String({ default: "http://localhost:8000" }),
   CONFIG_FILE: t.String({ default: "./config.yaml" }),
 });
